bc754e42b4660314890a7c712153da8966670a3f
geisse-costa <costageisse@gmail.com>
2023-09-14 18:06:25 -0300

    Atualização aulas

---
+++ CursoDankiCode/Assets/Scripts/AulaGetComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AulaGetComponent : MonoBehaviour
+{
+    public Rigidbody rig;
+    private BoxCollider box;
+
+    //private AulaClassPlayer player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        box = GetComponent<BoxCollider>();
+
+        //player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
